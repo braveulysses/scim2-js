74c729e1cc3d185fd3bb68b84d29cd5a5558dff2
Jacob Childress <jacobc@gmail.com>
2017-01-24 14:14:19 -0600

    Define the various SCIM 2 message URNs as constants.

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,9 @@
 module.exports = {
-  SCIM_MEDIA_TYPE: 'application/scim+json'
+  SCIM_MEDIA_TYPE: 'application/scim+json',
+  SCIM_SEARCH_REQUEST_URN: 'urn:ietf:params:scim:api:messages:2.0:SearchRequest',
+  SCIM_LIST_RESPONSE_URN: 'urn:ietf:params:scim:api:messages:2.0:ListResponse',
+  SCIM_PATCH_MESSAGE_URN: 'urn:ietf:params:scim:api:messages:2.0:PatchOp',
+  SCIM_BULK_REQUEST_MESSAGE_URN: 'urn:ietf:params:scim:api:messages:2.0:BulkRequest',
+  SCIM_BULK_RESPONSE_MESSAGE_URN: 'urn:ietf:params:scim:api:messages:2.0:BulkResponse',
+  SCIM_ERROR_MESSAGE_URN: 'urn:ietf:params:scim:api:messages:2.0:Error'
 };
