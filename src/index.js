ae1bd27ef8e68ca753197f79c9765e05ed625343
Jacob Childress <jacobc@gmail.com>
2017-02-10 16:01:07 -0600

    Fixed a bug in which functions from the Patch module weren't properly exported.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import Constants from './Constants';
+import * as Patch from './Patch';
 import Resource from './Resource';
 
 module.exports = {
   Constants: Constants,
+  Patch: Patch,
   Resource: Resource
 };
